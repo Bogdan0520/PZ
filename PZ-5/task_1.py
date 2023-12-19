d3e4507a7d0fc1653ec463c3913772f74b3e8165
Bogdan0520 <146367368+Bogdan0520@users.noreply.github.com>
2023-12-19 13:16:37 +0300

    Update task_1.py

--- PZ-5/task_1.py
+++ PZ-5/task_1.py
@@ -1,48 +1,25 @@
-def max2(max,min): 
-    if max > min: return max 
-    return min 
+a,b = int(input("Введите первое число: ")),int(input("Введите второе число: "))
+c = max(a,b)
+print("наибольшее число: ",c)
+y = int(input("Введите третье число: "))
+c = max(a,b,y)
+print("наибольшее число: ",c)
+b = int(input("Введите многоразрядное, целое число: "))
+t = 0
+while b>0:
+  t += 1
+  b//=10
+print(t)
+#Нахождение всех двузначных чисел, в которых есть заданная цифра
+def digit(n,a):
+  t= False
+  while n>10:
+    if n%10 == a:
+      t=True
+    n//=10
+  return t
 
-def max3(a,b,c): 
-    return max2(a, max2(b,c)) 
-
-a,b,c = int(input('Введи первое число: ')), int(input('Введи второе число: ')), int(input('Введи третье число: ')) 
-
-print('Максимальное число: ',max3(a,b,c))
-
-
-
-def countInt(k): 
-    t = 0 
-    while k > 0: 
-        k //= 10 
-        t += 1
-    return t 
-
-Int_Nuber = input("Введи целое число: ") 
-while type(Int_Nuber) != int: # обработка исключений 
-    try: 
-        Int_Nuber = int(Int_Nuber) 
-    except ValueError: 
-        print("Неправильно ввели!") 
-        Int_Nuber = input("Введите целое число: ") 
-        
-print('Количество цифр в цисле: ', countInt(Int_Nuber))
-    
-    
-def search_figure(k): 
-    d = 10 
-    while d != 100: 
-        a, b = divmod(d, 10) 
-        if (a == k) or (b == k): 
-                print(d, -d) 
-        d += 1
-
-figure = input("Введи целое число от 0 до +- 9: ") 
-while type(figure) != int: # обработка исключений 
-    try: 
-        figure = int(figure) 
-    except ValueError: 
-        print("Неправильно ввели!") 
-        figure = input("Введите целое число: ") 
-
-search_figure(figure)
+a = int(input("Введите цифру: "))
+for n in range(10,100):
+  if digit(n,a)==True:
+    print(n)
