9928dea99f245f9a65d3a46d2289698516b67d32
Bogdan0520 <146367368+Bogdan0520@users.noreply.github.com>
2023-12-21 11:50:33 +0300

    Update task_2_2.py

--- PZ-5/task_2_2.py
+++ PZ-5/task_2_2.py
@@ -1,17 +1,7 @@
-
-
 def rectangle_parameters(a,b):
-    if type(a) not in (int, float):
-        raise ValueError(f'{a} не является вещественным числом')
-    if type(b) not in (int, float):
-        raise ValueError(f'{b} не является вещественным числом')
-    
-    print(f'Периметр прямоугольника: {2*(a+b)}\n\
-Его площадь: {a*b}')
-    
-    
-#rectangle_parameters('banan', 4) #banan не является вещественным числом
-#rectangle_parameters(5, 'koshka') # koshka не является вещественным числом
+    return 2*(a+b), a*b
 
-rectangle_parameters(1,2) 
-rectangle_parameters(14.3, 5)
+a = int(input("Введите сторону a: "))
+b = int(input("Введите сторону b: "))
+per,sqw=rectangle_parameters(a, b)
+print('Периметр прямоугольника: ', per,'\nЕго площадь: ', sqw)
