09249a54e858b60b2f5a62fd5f527c0467054f1e
Bogdan0520 <146367368+Bogdan0520@users.noreply.github.com>
2023-12-21 11:53:28 +0300

    Update task_var_4_2.py

--- PZ-5/task_var_4_2.py
+++ PZ-5/task_var_4_2.py
@@ -1,19 +1,15 @@
-
-
-
-
-def RectPS(x1,x2,y1,y2):
-    for elem in [x1,x2,y1,y2]:
-        if type(elem) not in (int, float):
-            raise ValueError(f'{elem} не является вещественным числом')
+def RectPS(x1,x2,y1,y2, P,S):
     a = abs(x1-x2)
     b = abs(y1-y2)
-    print(f'Периметр прямоугольника: {2*(a+b)}\n\
-Его площадь: {a*b}')
-    
-#RectPS('banan',1,2,3) #ValueError: banan не является вещественным числом
-# RectPS(4,'koshka',2,3) #ValueError: koshka не является вещественным числом
+    P = 2*(a+b)
+    S = a*b
+    return P, S
 
-RectPS(1,2,3,4)
-RectPS(-1,-2,3,4)
-RectPS(16.6,92,-38,44)
+P=0
+S=0
+P,S = RectPS(1,2,3,4,P,S)
+print(f'Периметр прямоугольника: ',P ,'\n Его площадь: ',S )
+P,S = RectPS(-1,-2,3,4,P,S)
+print(f'Периметр прямоугольника: ',P ,'\n Его площадь: ',S )
+P,S = RectPS(16.6,92,-38,44,P,S)
+print(f'Периметр прямоугольника: ',P ,'\n Его площадь: ',S )
